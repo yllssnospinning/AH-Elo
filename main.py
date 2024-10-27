a0d85ce019d4c0cff664c44f44b67fdbfe9eb56f
yllssnospinning <mrnospinning@gmail.com>
2024-10-27 19:11:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
 from ahr import ahr
-import matplotlib.pyplot as plt
-import csv
 
-import pandas as pd
-from lightweight_charts import Chart
-
-
-
-
-def getohlc(prices, period):
-    o, h, l, c = 0, 0, 10000000, 0
-    num = 1
-    time = 1
-    writer.writerow(['time', 'open', 'high', 'low', 'close', 'volume'])
-    for i in prices:
-        if num == 1:
-            o = i
-        if i > h:
-            h = i
-        if i < l:
-            l = i
-        c = i
-        num += 1
-        t = '1/1/' + str(time + 2000)
-        if num > period:
-            writer.writerow([t, o, h, l, c, 1])
-            num = 1
-            time += 1
-            o, h, l, c = 0, 0, 10000000, 0
-    if num != 1:
-        writer.writerow([t, o, h, l, c, 1])
 
 sys = ahr()
-sys.loadGames('C:\\Users\\PC\\Desktop\\Programming\\whrV1\\scores.txt')
-inflation = []
+sys.loadGames('fileAddress')
 db = {}
 for i in range(0, len(sys.db) + 1):
 #for i in [len(sys.db)]:
@@ -46,20 +15,3 @@ for i in range(0, len(sys.db) + 1):
             db[ii] = [], []
         db[ii][0].append(i)
         db[ii][1].append(player[0])
-
-
-
-with open('data.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    getohlc(db['LYL'][1], 10)
-
-
-if __name__ == '__main__':
-    
-    chart = Chart()
-    
-    # Columns: time | open | high | low | close | volume 
-    df = pd.read_csv('data.csv')
-    chart.set(df)
-    
-    chart.show(block=True)
